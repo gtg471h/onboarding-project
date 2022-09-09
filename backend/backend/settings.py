ff23098c1ed54b1c868807a6221d3e502c471dc9
Chris Jordan <cjordan@zenput.com>
2022-09-09 10:52:11 -0400

    fic up secrets behavior for django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jsb$n2uh+qqnhoykmyrsq+^w0xai^r-_n&8j6h*9x#c1s2*3@y'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-jsb$n2uh+qqnhoykmyrsq+^w0xai^r-_n&8j6h*9x#c1s2*3@y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
